b65936ec1134ea9ac61e8d0a4024b3277369cc3a
Joshua Richardson <jrichardson@wearebond.com>
2024-05-16 14:04:11 -0600

    sample home page with mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { mockData } from '~/lib/mockData'
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-neutral-900 text-white">
-      <p>Packing app (in progress)</p>
+    <main className="flex min-h-screen flex-col p-4">
+      <div className="flex flex-wrap gap-4">
+        {mockData.images.map((image, index) => (
+          <img
+            key={index}
+            src={image.url}
+            alt={`Image ${index + 1}`}
+            className="w-48"
+          />
+        ))}
+      </div>
     </main>
   )
 }
