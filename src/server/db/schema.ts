4c85bdaddd0b8b98213c791d686b172d78a9c8b7
Joshua Richardson <jrichardson@wearebond.com>
2024-05-16 12:42:35 -0600

    update db schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,8 +1,7 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
-
-import { sql } from "drizzle-orm";
-import { index, int, sqliteTableCreator, text } from "drizzle-orm/sqlite-core";
+import { relations, sql } from 'drizzle-orm'
+import { index, int, sqliteTableCreator, text } from 'drizzle-orm/sqlite-core'
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -10,19 +9,36 @@ import { index, int, sqliteTableCreator, text } from "drizzle-orm/sqlite-core";
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = sqliteTableCreator((name) => `packing_${name}`);
+export const createTable = sqliteTableCreator(name => `packing_${name}`)
 
-export const posts = createTable(
-  "post",
+export const boxes = createTable(
+  'box',
   {
-    id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-    name: text("name", { length: 256 }),
-    createdAt: int("created_at", { mode: "timestamp" })
+    id: int('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+    name: text('name', { length: 256 }),
+    createdAt: int('created_at', { mode: 'timestamp' })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
-    updatedAt: int("updatedAt", { mode: "timestamp" }),
+    updatedAt: int('updatedAt', { mode: 'timestamp' }),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
+  example => ({
+    nameIndex: index('name_idx').on(example.name),
   })
-);
+)
+
+export const items = createTable('item', {
+  id: int('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+  name: text('name', { length: 256 }),
+  description: text('description', { length: 256 }),
+  image: text('src'),
+  createdAt: int('created_at', { mode: 'timestamp' })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: int('updatedAt', { mode: 'timestamp' }),
+})
+export const boxRelations = relations(boxes, ({ many }) => ({
+  items: many(items),
+}))
+export const itemRelations = relations(items, ({ one }) => ({
+  box: one(boxes),
+}))
