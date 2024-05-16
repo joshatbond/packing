b117e45ecf93ee81b9004c31dcb222c58e170f93
Joshua Richardson <jrichardson@wearebond.com>
2024-05-16 13:20:33 -0600

    add auth token to config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,13 @@
-import { type Config } from "drizzle-kit";
+import { type Config } from 'drizzle-kit'
 
-import { env } from "~/env";
+import { env } from '~/env'
 
 export default {
-  schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
+  schema: './src/server/db/schema.ts',
+  dialect: 'sqlite',
   dbCredentials: {
     url: env.DATABASE_URL,
+    authToken: env.DATABASE_AUTH_TOKEN,
   },
-  tablesFilter: ["packing_*"],
-} satisfies Config;
+  tablesFilter: ['packing_*'],
+} satisfies Config
